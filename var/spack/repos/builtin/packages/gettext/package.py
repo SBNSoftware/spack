4b0bc9ef9cb6f253374f78bd8c7e1906c2f1f324
Tingjun Yang <tjyang@fnal.gov>
2025-01-27 11:20:42 -0600

    Change configurations for gettext and grpc.

--- var/spack/repos/builtin/packages/gettext/package.py
+++ var/spack/repos/builtin/packages/gettext/package.py
@@ -154,16 +154,12 @@ class Gettext(AutotoolsPackage, GNUMirrorPackage):
 
     @property
     def libs(self):
-        # on redhat and clones, libintl is magic, you just want libc.
-        # and sometimes 32 bit libraries you don't want in are in /usr/lib.
-
-        if self.prefix == "/usr" and self.spec.os[:-1] in ["scientific", "redhat", "centos"]:
-            root = "/usr/lib64"
-            liblist = (["libasprintf", "libgettextlib", "libgettextpo", "libgettextsrc"],)
-        else:
-            root = self.prefix
-            liblist = (
-                ["libasprintf", "libgettextlib", "libgettextpo", "libgettextsrc", "libintl"],
-            )
-
-        return find_libraries(liblist, root=root, recursive=True)
+        # Do not fail if the installed gettext did not yet have the shared variant:
+        shared_variant = self.spec.variants.get("shared")
+        libs = find_libraries(
+            ["libasprintf", "libgettextlib", "libgettextpo", "libgettextsrc", "libintl"],
+            root=self.prefix,
+            recursive=True,
+            shared=True if not shared_variant else shared_variant.value,
+        )
+        return libs
